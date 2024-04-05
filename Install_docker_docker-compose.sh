68c89f3b26921d4cda22204418dd183c86874850
Mukul Malhotra <smilemukul2005@gmail.com>
2024-04-05 14:11:36 +0530

    Update Install_docker_docker-compose.sh

--- Install_docker_docker-compose.sh
+++ Install_docker_docker-compose.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 ## Update Centos 7
-sudo yum update -y
+sudo yum -y update && \
 
 ## Install Docker Engine on CentOS 7
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install -y yum-utils && \
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo && \
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && \
 
 ## Install Docker Compose on Centos 7
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker–compose --version
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && \
+sudo chmod +x /usr/local/bin/docker-compose && \
+docker–compose --version && \
 
 ## Start Docker Service
-sudo systemctl start docker
+sudo systemctl start docker && \
 sudo systemctl enable docker
