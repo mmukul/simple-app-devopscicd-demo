73233a8f48e84b34eb6cd8a9f287e7db8bac1cd9
Mukul Malhotra <smilemukul2005@gmail.com>
2024-04-05 14:12:04 +0530

    Update Install_Jenkins_script.sh

--- Install_Jenkins_script.sh
+++ Install_Jenkins_script.sh
@@ -1,6 +1,6 @@
-#/bin/bash
+#!/bin/bash
 
-## Install Jenkins on Centos7
+## Install Jenkins on Centos 7
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo && \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key && \
