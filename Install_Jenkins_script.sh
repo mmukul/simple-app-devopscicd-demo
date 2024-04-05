035874e4ca7b0d62e0fd7a66d007c3b39fa7af6e
Mukul Malhotra <smilemukul2005@gmail.com>
2024-04-05 14:08:56 +0530

    Update Install_Jenkins_script.sh

--- Install_Jenkins_script.sh
+++ Install_Jenkins_script.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-## Install Jenkins
+## Install Jenkins on Centos7
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo && \
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key && \
 
-sudo yum install fontconfig java-11-openjdk -y && \
-sudo yum install jenkins -y && \
+sudo yum install -y fontconfig java-11-openjdk && \
+sudo yum install -y jenkins && \
  
 sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp && \
 sudo firewall-cmd --reload
