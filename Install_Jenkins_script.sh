82fcfa4f05568ade7b3d6735371eff4f6ef869b2
Mukul Malhotra <smilemukul2005@gmail.com>
2024-04-14 15:30:10 +0530

    Update Install_Jenkins_script.sh

--- Install_Jenkins_script.sh
+++ Install_Jenkins_script.sh
@@ -3,7 +3,7 @@
 SRE=`cat /var/lib/jenkins/secrets/initialAdminPassword`
 
 
-## Remove Java and Jenkins
+## Remove Java and Jenkins from Master
 
 yum remove -y java*
 yum remove -y jenkins
@@ -11,7 +11,7 @@ rm -rf /etc/yum.repos.d/jenkins.repo
 
 
 
-# Install on Jenkins Master Node
+## Install on Jenkins Master Node
 yum install -y vim wget epel-release curl java-11-openjdk-devel
 
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -29,3 +29,11 @@ firewall-cmd --zone=public --add-port=8080/tcp --permanent
 sudo firewall-cmd --reload
 
 echo "Jenkins password is $SRE"
+
+
+## Install Java on Slave node
+
+yum update -y
+yum install java-11-openjdk-devel -y
+systemctl stop firewalld
+systemctl disable firewalld
