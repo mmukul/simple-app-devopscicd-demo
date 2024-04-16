c19e0c251db8b8fc0255e4b1f23a7eb03e84a2ac
Mukul Malhotra <smilemukul2005@gmail.com>
2024-04-16 16:35:20 +0530

    maven installation script

---
+++ Install_maven.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+wget http://mirrors.ibiblio.org/apache/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz -P /opt
+cd /opt
+
+tar xzvf apache-maven-3.9.6-bin.tar.gz
+
+sudo ln -s /opt/apache-maven-3.9.6 /opt/maven
+
+cat <<EOF | sudo tee /etc/profile.d/maven.sh
+export JAVA_HOME=/usr/lib/jvm/jre-openjdk
+export MAVEN_HOME=/opt/maven
+export PATH=${MAVEN_HOME}/bin:${PATH}
+EOF
+
+sudo chmod +x /etc/profile.d/maven.sh
+
+source /etc/profile.d/maven.sh
