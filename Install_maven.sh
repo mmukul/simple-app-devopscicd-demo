083cc125d9daa94cbc2d967937842a4f8cae6733
Mukul Malhotra <smilemukul2005@gmail.com>
2024-04-29 12:43:32 +0530

    Update Install_maven.sh

--- Install_maven.sh
+++ Install_maven.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run on Jenkins Master and Jenkins Slave
+# Run on Jenkins Master and Jenkins Slave using CLI on Linux Terminal
 
 wget http://mirrors.ibiblio.org/apache/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz -P /opt
 cd /opt
@@ -18,3 +18,5 @@ EOF
 sudo chmod +x /etc/profile.d/maven.sh
 
 source /etc/profile.d/maven.sh
+
+# Configure Maven and JDK setting on Jenkins Console
