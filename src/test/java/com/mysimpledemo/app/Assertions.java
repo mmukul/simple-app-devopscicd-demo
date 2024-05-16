33e2cf1b6af39c64bc7eecc8f845370e840a20fb
Mukul Malhotra <smilemukul2005@gmail.com>
2024-05-16 20:14:21 +0530

    update files

---
+++ src/test/java/com/mysimpledemo/app/Assertions.java
@@ -0,0 +1,10 @@
+package com.mysimpledemo.app;
+
+public class Assertions {
+
+    public static void assertTrue(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'assertTrue'");
+    }
+
+}
---
+++ src/test/java/com/mysimpledemo/app/Test.java
@@ -0,0 +1,5 @@
+package com.mysimpledemo.app;
+
+public @interface Test {
+
+}
